5a547dc0a2b5a54105a77df25a5f01a5628867db
ArkhipovVS <ArkhipovVS@imdone.ru>
2018-03-19 20:45:28 +0300

    Update Palindrome.java

--- Palindrome.java
+++ Palindrome.java
@@ -1,25 +1,24 @@
 import java.util.*;
 
 public class Palindrome{
-
 	public static void main(String[] args){
-			Scanner scan = new Scanner(System.in);
-			String s = scan.next();
-			isPalindrome(s);
+		Scanner scan = new Scanner(System.in);
+		String s = scan.next();
+		isPalindrome(s);
 	}
-    public static String reverseString(String s){
-        String r = "";
-        for (int i = s.length() - 1; i >= 0; --i)
-            r += s.charAt(i);
-        return r;        
-    }
-    public static Boolean isPalindrome(String s) {
-        if(s.equals(reverseString(s))){
-            System.out.println("is Palindrome");
-        }
-        else{
-            System.out.println("not Palindrome");
-        }
-        return s.equals(reverseString(s));        
-    }
+	public static String reverseString(String s){
+		String r = "";
+		for (int i = s.length() - 1; i >= 0; --i)
+			r += s.charAt(i);
+		return r;        
+	}
+	public static Boolean isPalindrome(String s) {
+		if(s.equals(reverseString(s))){
+			System.out.println("is Palindrome");
+		}
+		else{
+			System.out.println("not Palindrome");
+		}
+		return s.equals(reverseString(s));        
+	}
 }
